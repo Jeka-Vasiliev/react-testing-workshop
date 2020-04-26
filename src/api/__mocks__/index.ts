ef9db805662c629457b10821961ebc3d2f4bfbfa
Jeka Vasilyev <zheka_v@inbox.ru>
2020-04-26 14:05:25 +0300

    unconstrained mock for api

---
+++ src/api/__mocks__/index.ts
@@ -0,0 +1,32 @@
+import Todo from "../../types/Todo";
+
+let id = 1;
+let db: Todo[] = [];
+
+export default {
+  async add(title: string) {
+    const newTodo = {
+      id,
+      order: id,
+      title,
+      url: `fake://todo/${id}`,
+      completed: false,
+    };
+    id++;
+    db = [...db, newTodo];
+    return newTodo;
+  },
+  async fetch() {
+    return db;
+  },
+  async delete(todo: Todo) {
+    db = db.filter((t) => t.id !== todo.id);
+  },
+  async update(todo: Todo) {
+    db = db.reduce((acc, next) => {
+      const possiblyUpdatedNext =
+        next.id === todo.id ? { ...next, todo } : next;
+      return [...acc, possiblyUpdatedNext];
+    }, [] as Todo[]);
+  },
+};
