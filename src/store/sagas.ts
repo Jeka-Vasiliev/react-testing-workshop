d8239f4cfd805d6c8ae8d848fd87917c070eae29
Jeka Vasilyev <zheka_v@inbox.ru>
2020-04-26 13:24:25 +0300

    error handling for sagas

--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -5,32 +5,48 @@ import { addTodo, deleteTodo, loadTodos, toggleTodo } from './actions';
 
 function* listenLoadTodos() {
   yield takeLatest(loadTodos.request, function* () {
-    const response = yield call(api.fetch);
-    yield put(loadTodos.success(response.data));
+    try {
+      const response = yield call(api.fetch);
+      yield put(loadTodos.success(response.data));
+    } catch (error) {
+      yield put(loadTodos.failure(error));
+    }
   });
 }
 
 function* listenAddTodo() {
   yield takeEvery(addTodo.request, function* ({ payload: title }) {
-    const response = yield call(api.add, title);
-    yield put(addTodo.success(response.data));
+    try {
+      const response = yield call(api.add, title);
+      yield put(addTodo.success(response.data));
+    } catch (error) {
+      yield put(addTodo.failure(error));
+    }
   });
 }
 
 function* listenToggleTodo() {
   yield takeEvery(toggleTodo.request, function* ({ payload: todo }) {
-    const response = yield call(api.update, {
-      ...todo,
-      completed: !todo.completed,
-    });
-    yield put(toggleTodo.success(response.data));
+    try {
+      const response = yield call(api.update, {
+        ...todo,
+        completed: !todo.completed,
+      });
+      yield put(toggleTodo.success(response.data));
+    } catch (error) {
+      yield put(toggleTodo.failure(error));
+    }
   });
 }
 
 function* listenDeleteTodo() {
   yield takeEvery(deleteTodo.request, function* ({ payload: todo }) {
-    yield call(api.delete, todo);
-    yield put(deleteTodo.success(todo));
+    try {
+      yield call(api.delete, todo);
+      yield put(deleteTodo.success(todo));
+    } catch (error) {
+      yield put(deleteTodo.failure(error));
+    }
   });
 }
 
